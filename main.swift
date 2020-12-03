d8f689744970f7e8d776469d2265a8100853e743
Kim_Min_Jae <rlaalswo201@gmail.com>
2020-12-03 17:03:10 +0000

    var and let

--- main.swift
+++ main.swift
@@ -3,3 +3,17 @@ import Swift
 let age : Int = 10
 
 print("안녕하세요 저는 \(age) 살입니다")
+
+class Person {
+    var name: String = "name"
+    var age : Int = 10
+}
+
+let minjae : Person = Person()
+
+print(minjae)
+print(minjae.name)
+print(minjae.age)
+
+minjae.name = "minjae"
+print(minjae.name)
